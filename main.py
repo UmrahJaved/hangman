0c0ddd3fa6ccb13c8899aafa5f93d6a661f9c78f
Umrah Javed <umrahjaved.1@gmail.com>
2022-01-26 15:44:39 +0100

    Done

---
+++ main.py
@@ -0,0 +1,12 @@
+from utils.game import Hangman
+import random
+
+
+possible_words = ["Becode", "Learning", "Mathematics", "Sessions", "Computer", "Machine", "Bruxelles", "Belgium"]
+word_to_find = list(random.choice(possible_words).upper())
+
+objHangman=Hangman(word_to_find)
+
+funcPlayReturn=objHangman.play(word_to_find)
+objHangman.game_over(funcPlayReturn[2])
+objHangman.well_played(funcPlayReturn[0],funcPlayReturn[4],funcPlayReturn[3])
