c96a392723da2454676d49b4b8bfe80729370bca
Umrah Javed <umrahjaved.1@gmail.com>
2022-01-26 16:19:14 +0100

    Final

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ word_to_find = list(random.choice(possible_words).upper())
 
 objHangman=Hangman(word_to_find)
 
-funcPlayReturn=objHangman.play(word_to_find)
-objHangman.game_over(funcPlayReturn[2])
-objHangman.well_played(funcPlayReturn[0],funcPlayReturn[4],funcPlayReturn[3])
+#funcPlayReturn=objHangman.play(word_to_find)
+#objHangman.game_over(funcPlayReturn[2])
+#objHangman.well_played(funcPlayReturn[0],funcPlayReturn[4],funcPlayReturn[3])
+
+def start_game():
+    funcPlayReturn = objHangman.play(word_to_find)
+    objHangman.game_over(funcPlayReturn[2])
+    objHangman.well_played(funcPlayReturn[0], funcPlayReturn[4], funcPlayReturn[3])
+
+start_game()
